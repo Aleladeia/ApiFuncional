f787239ffee617400bd28001e51ee8df23be7e4f
Alexandre Jose <aleladeia99@gmail.com>
2024-05-06 17:04:55 -0300

    Add project files.

---
+++ Data/ApiDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ApiFuncional.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiFuncional.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Produto> Produtos{ get; set; }
+    }
+}
