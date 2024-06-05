8eeffebc614ca5311eaac5c02eeb60e3b6355edb
Alexandre Jose <aleladeia99@gmail.com>
2024-06-05 10:14:26 -0300

    Implementado Controller de autenticação com endpoints de registro e login para gerar o token de autenticação

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,86 @@
+﻿using ApiFuncional.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace ApiFuncional.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/conta")]
+    public class AuthController : ControllerBase
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly JwtSettings _jwtSettings;
+
+        public AuthController(SignInManager<IdentityUser> signInManager,
+                              UserManager<IdentityUser> userManager,
+                              IOptions<JwtSettings> jwtSettings) //preciso passar esse IOptions porque ele é configurado como Options
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _jwtSettings = jwtSettings.Value;
+        }
+
+        [HttpPost("registrar")]
+        public async Task<IActionResult> Registrar(RegisterUserViewModel registerUser) //criar usuário
+        {
+            if(!ModelState.IsValid) return ValidationProblem(ModelState);
+
+            var user = new IdentityUser
+            {
+                UserName = registerUser.Email,
+                Email = registerUser.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, registerUser.Password);
+
+            if(result.Succeeded) 
+            {
+                await _signInManager.SignInAsync(user, false);
+                return Ok(GerarJwt());
+            }
+
+            return Problem("Falha ao registrar o usuário");
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginUserViewModel loginUser) //autenticar usuário
+        {
+            if (!ModelState.IsValid) return ValidationProblem(ModelState);
+
+            var result = await _signInManager.PasswordSignInAsync(loginUser.Email, loginUser.Password, false, true);
+
+            if(result.Succeeded)
+            {
+                return Ok(GerarJwt());
+            }
+
+            return Problem("Usuário ou senha incorretos");
+        }
+
+        private string GerarJwt()
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_jwtSettings.Segredo);
+
+            var token = tokenHandler.CreateToken(new SecurityTokenDescriptor
+            {
+                Issuer = _jwtSettings.Emissor,
+                Audience = _jwtSettings.Audiencia,
+                Expires = DateTime.UtcNow.AddHours(_jwtSettings.ExpiracaoHoras),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            });
+
+            var encodedToken = tokenHandler.WriteToken(token);
+
+            return encodedToken;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 using ApiFuncional.Data;
+using ApiFuncional.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,6 +30,31 @@ builder.Services.AddDbContext<ApiDbContext>(options =>
 builder.Services.AddIdentity<IdentityUser, IdentityRole>() 
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApiDbContext>();
+
+//Pegando o Token e gerando a chave encodada
+var JwtSettingsSection = builder.Configuration.GetSection("JwtSettings");
+builder.Services.Configure<JwtSettings>(JwtSettingsSection);
+
+var jwtSettings = JwtSettingsSection.Get<JwtSettings>();
+var key = Encoding.ASCII.GetBytes(jwtSettings.Segredo);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = true;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = jwtSettings.Audiencia,
+        ValidIssuer = jwtSettings.Emissor
+    };
+});
 
 //Definindo o comportamento da nossa App daqui pra baixo
 var app = builder.Build(); 
